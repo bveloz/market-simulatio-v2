265ec0d49f28087c0f12b6940dd1c94452c59e6e
Braulio Veloz <bzveloz4@gmail.com>
2024-12-08 15:17:55 -0500

    fine tune

--- mSim.c
+++ mSim.c
@@ -4,16 +4,13 @@
 #include <time.h>
 #include <string.h>
 
-#ifndef M_PI
-#define M_PI 3.14159
-#endif
+
 #define NUM_CUSTOMERS 1500
 #define NUM_DAYS 60
 #define GAS_TANK_CAPACITY 15.0
 #define DAILY_TRAVEL_HOURS 2.0
 #define GAS_CONSUMPTION_RATE 2.0
 #define MAX_GAS_STATIONS 10
-#define MAX_CONVENIENCE_SCORE 5
 #define JSON_FILE "simulation_results.json"
 
 
@@ -23,9 +20,8 @@ typedef struct {
     double adjusted_price;
     int travel_time_from_a;
     int travel_time_from_b;
-    int convenience_score_a_to_b;
-    int convenience_score_b_to_a;
 } GasStation;
+
 
 // Customer struct
 typedef struct {
@@ -57,43 +53,51 @@ void simulate_customer_trip(Customer *customer, GasStation *stations, int num_st
         printf("Error: Gas consumption exceeds tank capacity.\n");
         exit(EXIT_FAILURE);
     }
+    for (int trip = 0; trip < 2; trip++)   
+    {
+        // Determine if refueling is needed
+        if ((customer->purchase_style == 0 && current_gas <= 3) || // Low tank (3 gallons)
+            (customer->purchase_style == 1 && current_gas < GAS_TANK_CAPACITY / 2) || // Half tank
+            (customer->purchase_style == 2)) { // Low cost check
+            int selected_station = -1;
+            double min_cost = 1e9;
+            double min_time = 1e9;
+            int trip_travel_time;
+            for (int i = 0; i < num_stations; i++) {
+                stations[i].adjusted_price = bell_curve_price(stations[i].base_price);
 
-    // Determine if refueling is needed
-    if ((customer->purchase_style == 0 && current_gas <= 3) || // Low tank (3 gallons)
-        (customer->purchase_style == 1 && current_gas < GAS_TANK_CAPACITY / 2) || // Half tank
-        (customer->purchase_style == 2)) { // Low cost check
-        int selected_station = -1;
-        double min_cost = 1e9;
-        double min_time = 1e9;
-        for (int i = 0; i < num_stations; i++) {
-            stations[i].adjusted_price = bell_curve_price(stations[i].base_price);
+                int temp_travel_time = trip % 2 == 0 ? stations[i].travel_time_from_a : stations[i].travel_time_from_b; //alternate trip times between trips
+                double cost = stations[i].adjusted_price * (GAS_TANK_CAPACITY - current_gas);
+                double time = temp_travel_time;
 
-            double cost = stations[i].adjusted_price * (GAS_TANK_CAPACITY - current_gas);
-            double time = stations[i].travel_time_from_a + stations[i].convenience_score_a_to_b;
+                if (customer->purchase_style == 0 && time < min_time) {
+                    min_time = time;
+                    trip_travel_time = temp_travel_time;
+                    selected_station = i;
+                } else if (customer->purchase_style == 1 && time < min_time) {
+                    min_time = time;
+                    trip_travel_time = temp_travel_time;
+                    selected_station = i;
+                } else if (customer->purchase_style == 2 && cost < min_cost) {
+                    min_cost = cost;
+                    trip_travel_time = temp_travel_time;
+                    selected_station = i;
+                }
+            }
 
-            if (customer->purchase_style == 0 && time < min_time) {
-                min_time = time;
-                selected_station = i;
-            } else if (customer->purchase_style == 1 && time < min_time) {
-                min_time = time;
-                selected_station = i;
-            } else if (customer->purchase_style == 2 && cost < min_cost) {
-                min_cost = cost;
-                selected_station = i;
+            // Refuel at the selected station
+            if (selected_station != -1) {
+                customer->gas_tank_quantity = GAS_TANK_CAPACITY;
+                customer->total_expenses_per_day[day] += stations[selected_station].adjusted_price * (GAS_TANK_CAPACITY - current_gas);
+                customer->total_time_spent[day] += trip_travel_time;
             }
         }
-
-        // Refuel at the selected station
-        if (selected_station != -1) {
-            customer->gas_tank_quantity = GAS_TANK_CAPACITY;
-            customer->total_expenses_per_day[day] += stations[selected_station].adjusted_price * (GAS_TANK_CAPACITY - current_gas);
-            customer->total_time_spent[day] += stations[selected_station].travel_time_from_a + stations[selected_station].convenience_score_a_to_b;
+        else
+        {
+            customer->gas_tank_quantity = current_gas;
         }
     }
-    else
-    {
-        customer->gas_tank_quantity = current_gas;
-    }
+    
 
     // Record daily metrics
     customer->average_cost_per_gallon[day] = customer->total_expenses_per_day[day] / (GAS_TANK_CAPACITY - current_gas);
@@ -105,16 +109,14 @@ void generate_gas_stations(GasStation *stations, int num_stations) {
         stations[i].base_price = 2.0 + ((double)rand() / RAND_MAX) * 1.0;
         stations[i].travel_time_from_a = rand() % 11 + 5; // 5 to 15 minutes
         stations[i].travel_time_from_b = rand() % 11 + 5; // 5 to 15 minutes
-        stations[i].convenience_score_a_to_b = rand() % (MAX_CONVENIENCE_SCORE + 1);
-        stations[i].convenience_score_b_to_a = rand() % (MAX_CONVENIENCE_SCORE + 1);
     }
 }
 
 // Main simulation function
 void run_simulation() {
     srand(time(NULL));
-
-    GasStation stations[MAX_GAS_STATIONS];
+    
+    
     Customer customers[NUM_CUSTOMERS];
     FILE *file = fopen(JSON_FILE, "w");
 
@@ -127,6 +129,9 @@ void run_simulation() {
 
     // Initialize customers
     for (int i = 0; i < NUM_CUSTOMERS; i++) {
+
+        
+
         customers[i].gas_tank_quantity = GAS_TANK_CAPACITY;
         customers[i].purchase_style = i % 3;
         memset(customers[i].total_expenses_per_day, 0, sizeof(customers[i].total_expenses_per_day));
@@ -138,12 +143,15 @@ void run_simulation() {
     // Run simulation
     for (int i = 0; i < NUM_CUSTOMERS; i++) 
     {
-        generate_gas_stations(stations, MAX_GAS_STATIONS);
+        // generate stations on a per customer basis
+        int rand_num_stations = rand() % MAX_GAS_STATIONS;
+        GasStation stations[rand_num_stations];
+        generate_gas_stations(stations, rand_num_stations);
 
         // simulate customer on the same gas Station configuration for 90 days
         for(int day = 0; day < NUM_DAYS; day++)
         {
-            simulate_customer_trip(&customers[i], stations, MAX_GAS_STATIONS, day);
+            simulate_customer_trip(&customers[i], stations, rand_num_stations, day);
             double sum = 0; 
             if (day > 0)
                 sum = customers[i].total_cumulative_expenses[day - 1];
